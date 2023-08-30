777d4daf50332d00391baac9e8192b3eeba2619f
dieniferverch <142456047+dieniferverch@users.noreply.github.com>
2023-08-29 21:30:38 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,73 @@
+// 
+//let nome;
+//let corPreferida;
+//nome = prompt ("qual seu nome?");
+//corPreferida = prompt("qual sua cor preferida?");
+//console.log("a cor favorita de " + nome + " é " + corPreferida);
+//console.log(`a cor favorita de ${nome} é $(corPreferida`);
+
+//
+
+//const nome = "Vitor Hugo";
+//console.log(nome.length) 
+
+//const frase = "OieEeEee ! ";
+//const fraseMinuscula = frase.toLowerCase();
+// console.log (fraseMinuscula);
+
+
+//const frase = "OieEeEee ! ";
+//const fraseeMaiuscula= frase.toLocaleUpperCase();
+//console.log (fraseeMaiuscula);
+
+//const email = "  mika@gmail.com   ";
+//console.log(email.trim());
+
+//const frase = "Hoje comi cenoura";
+//console.log(frase.includes("cenoura"));
+//console.log(frase.includes("batata"));
+
+// const frase = "Hoje comi cenoura, adoro cenoura";
+//const novaFrase = frase.replaceAll("cenoura, "batata");
+// console.log (novaFrase);
+
+//let frase;
+//frase = prompt("crie uma frase?");
+//console.log (frase);
+//const fraseeMaiuscula= frase.toUpperCase();
+//console.log (fraseeMaiuscula);
+//console.log (frase.replaceAll("o","i"));
+//console.log(frase.length);
+
+//const listaDeCompras = ["batata", "alface", "queijo"];
+//const listaDeNumerosMega = [2, 13, 26, 35, 41, 60];
+//const meuArray = ["banana", 15, true];
+
+//const listaDeCompras = ["abacate", "banana", "tomate"];
+//const segundoItem =  listaDeCompras[2];
+
+//const raças = ["beagle", "salsicha", "caramelo", "pug", "labrador"];
+//numero = prompt ("insira um numero de o a 4");
+//console.log(raças[numero]);
+
+//const pokemon = ["bulbasauro", "squirtle", "charmander"];
+//console.log(pokemon.length);
+
+//const seriesBoas = ["The Big Bang Theory, "Broolyn Nine-nine"];
+//console.log(seriesBoas.includes("The Big Bang Theory"));
+//console.log(seriesBoas.includes("Game of Thrones"));
+
+//const numeros = [1, 2, 3];
+//numeros.push(4);
+//console.log(numeros);
+//numeros.push(5,6,7);
+//console.log(numeros);
+
+//const meusPeixes = ["palhaco", "mandarim", "esturjão"];
+//meusPeixes.pop();
+//console.log(meusPeixes);
+
+const letras = ["A", "B", "C", "D", "E", "F","G", "H"];
+letras.splice(2, 1);
+letras.splice(3, 2);
+
